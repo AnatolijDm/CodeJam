e8e89251ae69116f564127600cadd9c2120c6492
AnatolijDm <anat_dr25@rambler.ru>
2019-11-06 00:50:16 +0300

    Init:made_keys.js

---
+++ keys.js
@@ -0,0 +1,337 @@
+var d = document.createElement('div');
+d.className = 'text';
+document.body.appendChild(d);
+
+var k = document.createElement('div');
+k.className = 'keys';
+document.body.appendChild(k);
+
+var row1 = document.createElement('div');
+row1.className = 'row';
+k.appendChild(row1);
+
+var q = document.createElement('div');
+q.className = 'keyQ';
+q.innerHTML = '`'
+row1.appendChild(q);
+
+var q1 = document.createElement('div');
+q1.className = 'keyQ';
+q1.innerHTML = '1'
+row1.appendChild(q1);
+
+var q2 = document.createElement('div');
+q2.className = 'keyQ';
+q2.innerHTML = '2'
+row1.appendChild(q2);
+
+var q3 = document.createElement('div');
+q3.className = 'keyQ';
+q3.innerHTML = '3'
+row1.appendChild(q3);
+
+var q4 = document.createElement('div');
+q4.className = 'keyQ';
+q4.innerHTML = '4'
+row1.appendChild(q4);
+
+var q5 = document.createElement('div');
+q5.className = 'keyQ';
+q5.innerHTML = '5'
+row1.appendChild(q5);
+
+var q6 = document.createElement('div');
+q6.className = 'keyQ';
+q6.innerHTML = '6'
+row1.appendChild(q6);
+
+var q7 = document.createElement('div');
+q7.className = 'keyQ';
+q7.innerHTML = '7'
+row1.appendChild(q7);
+
+var q8 = document.createElement('div');
+q8.className = 'keyQ';
+q8.innerHTML = '8'
+row1.appendChild(q8);
+
+var q9 = document.createElement('div');
+q9.className = 'keyQ';
+q9.innerHTML = '9'
+row1.appendChild(q9);
+
+var q0 = document.createElement('div');
+q0.className = 'keyQ';
+q0.innerHTML = '0'
+row1.appendChild(q0);
+
+var q01 = document.createElement('div');
+q01.className = 'keyQ';
+q01.innerHTML = '-'
+row1.appendChild(q01);
+
+var q02 = document.createElement('div');
+q02.className = 'keyQ';
+q02.innerHTML = '='
+row1.appendChild(q02);
+
+var q03 = document.createElement('div');
+q03.className = 'keyB';
+q03.innerHTML = 'Backspace'
+row1.appendChild(q03);
+
+var row2 = document.createElement('div');
+row2.className = 'row';
+k.appendChild(row2);
+
+var a = document.createElement('div');
+a.className = 'tab';
+a.innerHTML = 'TAB'
+row2.appendChild(a);
+
+var a1 = document.createElement('div');
+a1.className = 'keyQ';
+a1.innerHTML = 'Q'
+row2.appendChild(a1);
+
+var a2 = document.createElement('div');
+a2.className = 'keyQ';
+a2.innerHTML = 'W'
+row2.appendChild(a2);
+
+var a3 = document.createElement('div');
+a3.className = 'keyQ';
+a3.innerHTML = 'E'
+row2.appendChild(a3);
+
+var a4 = document.createElement('div');
+a4.className = 'keyQ';
+a4.innerHTML = 'R';
+row2.appendChild(a4);
+
+var a5 = document.createElement('div');
+a5.className = 'keyQ';
+a5.innerHTML = 'T'
+row2.appendChild(a5);
+
+var a6 = document.createElement('div');
+a6.className = 'keyQ';
+a6.innerHTML = 'Y'
+row2.appendChild(a6);
+
+var a7 = document.createElement('div');
+a7.className = 'keyQ';
+a7.innerHTML = 'U'
+row2.appendChild(a7);
+
+var a8 = document.createElement('div');
+a8.className = 'keyQ';
+a8.innerHTML = 'I'
+row2.appendChild(a8);
+
+var a9 = document.createElement('div');
+a9.className = 'keyQ';
+a9.innerHTML = 'O'
+row2.appendChild(a9);
+
+var a0 = document.createElement('div');
+a0.className = 'keyQ';
+a0.innerHTML = 'P'
+row2.appendChild(a0);
+
+var a01 = document.createElement('div');
+a01.className = 'keyQ';
+a01.innerHTML = '['
+row2.appendChild(a01);
+
+var a02 = document.createElement('div');
+a02.className = 'keyQ';
+a02.innerHTML = ']'
+row2.appendChild(a02);
+
+var a03 = document.createElement('div');
+a03.className = 'enter';
+a03.innerHTML = 'ENTER'
+row2.appendChild(a03);
+
+var row3 = document.createElement('div');
+row3.className = 'row';
+k.appendChild(row3);
+
+var b = document.createElement('div');
+b.className = 'keyB';
+b.innerHTML = 'Caps Lock'
+row3.appendChild(b);
+
+var b1 = document.createElement('div');
+b1.className = 'keyQ';
+b1.innerHTML = 'A'
+row3.appendChild(b1);
+
+var b2 = document.createElement('div');
+b2.className = 'keyQ';
+b2.innerHTML = 'S'
+row3.appendChild(b2);
+
+var b3 = document.createElement('div');
+b3.className = 'keyQ';
+b3.innerHTML = 'D'
+row3.appendChild(b3);
+
+var b4 = document.createElement('div');
+b4.className = 'keyQ';
+b4.innerHTML = 'F';
+row3.appendChild(b4);
+
+var b5 = document.createElement('div');
+b5.className = 'keyQ';
+b5.innerHTML = 'G'
+row3.appendChild(b5);
+
+var b6 = document.createElement('div');
+b6.className = 'keyQ';
+b6.innerHTML = 'H'
+row3.appendChild(b6);
+
+var b7 = document.createElement('div');
+b7.className = 'keyQ';
+b7.innerHTML = 'J'
+row3.appendChild(b7);
+
+var b8 = document.createElement('div');
+b8.className = 'keyQ';
+b8.innerHTML = 'K'
+row3.appendChild(b8);
+
+var b9 = document.createElement('div');
+b9.className = 'keyQ';
+b9.innerHTML = 'L'
+row3.appendChild(b9);
+
+var b0 = document.createElement('div');
+b0.className = 'keyQ';
+b0.innerHTML = ':'
+row3.appendChild(b0);
+
+var b01 = document.createElement('div');
+b01.className = 'keyQ';
+b01.innerHTML = '"'
+row3.appendChild(b01);
+
+var b02 = document.createElement('div');
+b02.className = 'keyQ';
+b02.innerHTML = '|';
+row3.appendChild(b02);
+
+var b03 = document.createElement('div');
+b03.className = 'keyQ';
+b03.innerHTML = '~';
+row3.appendChild(b03);
+
+var row4 = document.createElement('div');
+row4.className = 'row';
+k.appendChild(row4);
+
+var c = document.createElement('div');
+c.className = 'keyB';
+c.innerHTML = 'Shift'
+row4.appendChild(c);
+
+var c1 = document.createElement('div');
+c1.className = 'keyQ';
+c1.innerHTML = 'Z'
+row4.appendChild(c1);
+
+var c2 = document.createElement('div');
+c2.className = 'keyQ';
+c2.innerHTML = 'X'
+row4.appendChild(c2);
+
+var c3 = document.createElement('div');
+c3.className = 'keyQ';
+c3.innerHTML = 'C'
+row4.appendChild(c3);
+
+var c4 = document.createElement('div');
+c4.className = 'keyQ';
+c4.innerHTML = 'V';
+row4.appendChild(c4);
+
+var c5 = document.createElement('div');
+c5.className = 'keyQ';
+c5.innerHTML = 'B'
+row4.appendChild(c5);
+
+var c6 = document.createElement('div');
+c6.className = 'keyQ';
+c6.innerHTML = 'N'
+row4.appendChild(c6);
+
+var c7 = document.createElement('div');
+c7.className = 'keyQ';
+c7.innerHTML = 'M'
+row4.appendChild(c7);
+
+var c8 = document.createElement('div');
+c8.className = 'keyQ';
+c8.innerHTML = '<'
+row4.appendChild(c8);
+
+var c9 = document.createElement('div');
+c9.className = 'keyQ';
+c9.innerHTML = '>'
+row4.appendChild(c9);
+
+var c0 = document.createElement('div');
+c0.className = 'keyQ';
+c0.innerHTML = '?'
+row4.appendChild(c0);
+
+var c01 = document.createElement('div');
+c01.className = 'keyQ';
+c01.innerHTML = '*'
+row4.appendChild(c01);
+
+var c02 = document.createElement('div');
+c02.className = 'keyQ';
+c02.innerHTML = '+';
+row4.appendChild(c02);
+
+var c03 = document.createElement('div');
+c03.className = 'keyQ';
+c03.innerHTML = '_';
+row4.appendChild(c03);
+
+var row5 = document.createElement('div');
+row5.className = 'row';
+k.appendChild(row5);
+
+var d = document.createElement('div');
+d.className = 'keyB';
+d.innerHTML = 'Ctr'
+row5.appendChild(d);
+
+var d1 = document.createElement('div');
+d1.className = 'keyQ';
+d1.innerHTML = 'Win'
+row5.appendChild(d1);
+
+var d2 = document.createElement('div');
+d2.className = 'keyQ';
+d2.innerHTML = 'Alt'
+row5.appendChild(d2);
+
+var d3 = document.createElement('div');
+d3.className = 'keySpace';
+d3.innerHTML = 'SPACE'
+row5.appendChild(d3);
+
+var d4 = document.createElement('div');
+d4.className = 'keyQ';
+d4.innerHTML = 'Alt';
+row5.appendChild(d4);
+
+var d5 = document.createElement('div');
+d5.className = 'keyB';
+d5.innerHTML = 'Ctr'
+row5.appendChild(d5);
