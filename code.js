4fce04e3525b179766a45c1b76d6969e14fb4347
AnatolijDm <anat_dr25@rambler.ru>
2019-11-07 00:26:27 +0300

    Feat: made_keys_dynamic

--- code.js
+++ code.js
@@ -3,18 +3,177 @@
 let isEnabled = true;
 let textA ="";
 
-document.querySelector('.keyQ').addEventListener('click', function() {
+let idKey;
+let textCl;
+
+function clickKey(idKey, textCl) {
+document.getElementById(idKey).addEventListener('click', function() {
         if (isEnabled) {
-        textA = textA + '`';
+        textA = textA + textCl;
     document.getElementById("text").innerHTML = textA;
         }
     });
+}
 
+
+let key;
+let textK;
+    function pressKey(key, textK) {
     document.addEventListener('keydown', function(event) {
-        if (event.code == 'Backquote') {
+        /*if(event.code == 'CapsLock' && event.code == key) {
             if (isEnabled) {
-                textA = textA + '`';
-            document.getElementById("text").innerHTML = textA;
+                textA = textA + textK.toUpperCase();
+                document.getElementById("text").innerHTML = textA;
                 }
+            }  else */if (event.code == key) {
+            if (isEnabled) {
+                textA = textA + textK;
+            document.getElementById("text").innerHTML = textA;
+            }
         }
-      });
+      })
+    }
+
+   /* document.onkeydown = function(e) {
+               e = e || window.event;
+            if (e.CapsLock && e.keyCode == key) {
+              alert('Shift + A');
+            }
+            return true;
+          }*/
+        
+
+        document.addEventListener('keydown', function(event) {
+            if (event.code == 'Backspace') {
+                if (isEnabled) {
+                    textA = textA.slice(0, -1);
+                document.getElementById("text").innerHTML = textA;
+                    }
+            }
+          });
+
+          document.getElementById('q03').addEventListener('click', function() {
+            if (isEnabled) {
+            textA =  textA.slice(0, -1);
+        document.getElementById("text").innerHTML = textA;
+            }
+        });
+
+          clickKey('q', '`');
+          clickKey('q1', '1');
+          clickKey('q2', '2');
+          clickKey('q3', '3');
+          clickKey('q4', '4');
+          clickKey('q5', '5');
+          clickKey('q6', '6');
+          clickKey('q7', '7');
+          clickKey('q8', '8');
+          clickKey('q9', '9');
+          clickKey('q0', '0');
+          clickKey('q01', '-');
+          clickKey('q02', '=');
+
+          clickKey('a', ' '+' '+' ');
+          clickKey('a1', 'q');
+          clickKey('a2', 'w');
+          clickKey('a3', 'e');
+          clickKey('a4', 'r');
+          clickKey('a5', 't');
+          clickKey('a6', 'y');
+          clickKey('a7', 'u');
+          clickKey('a8', 'i');
+          clickKey('a9', 'o');
+          clickKey('a0', 'p');
+          clickKey('a01', '[');
+          clickKey('a02', ']');
+    
+          clickKey('a03', '\n');
+    
+          clickKey('b1', 'a');
+          clickKey('b2', 's');
+          clickKey('b3', 'd');
+          clickKey('b4', 'f');
+          clickKey('b5', 'g');
+          clickKey('b6', 'h');
+          clickKey('b7', 'j');
+          clickKey('b8', 'k');
+          clickKey('b9', 'l');
+          clickKey('b0', ';');
+          clickKey('b01', '"');
+          clickKey('b02', '|');
+          clickKey('b03', '~');
+          
+          clickKey('c1', 'z');
+          clickKey('c2', 'x');
+          clickKey('c3', 'c');
+          clickKey('c4', 'v');
+          clickKey('c5', 'b');
+          clickKey('c6', 'n');
+          clickKey('c7', 'm');
+          clickKey('c8', '<');
+          clickKey('c9', '>');
+          clickKey('c0', '?');
+          clickKey('c01', '*');
+          clickKey('c02', '+');
+          clickKey('c03', '_');
+    
+          clickKey('d3', ' ');
+
+
+      pressKey('Backquote', '`');
+      pressKey('Digit1', '1');
+      pressKey('Digit2', '2');
+      pressKey('Digit3', '3');
+      pressKey('Digit4', '4');
+      pressKey('Digit5', '5');
+      pressKey('Digit6', '6');
+      pressKey('Digit7', '7');
+      pressKey('Digit8', '8');
+      pressKey('Digit9', '9');
+      pressKey('Digit0', '0');
+      pressKey('Minus', '-');
+      pressKey('Tab', ' '+' '+' ');
+      pressKey('KeyQ', 'q');
+      pressKey('KeyW', 'w');
+      pressKey('KeyE', 'e');
+      pressKey('KeyR', 'r');
+      pressKey('KeyT', 't');
+      pressKey('KeyY', 'y');
+      pressKey('KeyU', 'u');
+      pressKey('KeyI', 'i');
+      pressKey('KeyO', 'o');
+      pressKey('KeyP', 'p');
+      pressKey('BracketLeft', '[');
+      pressKey('BracketRight', ']');
+
+      pressKey('Enter', '\n');
+
+      pressKey('KeyA', 'a');
+      pressKey('KeyS', 's');
+      pressKey('KeyD', 'd');
+      pressKey('KeyF', 'f');
+      pressKey('KeyG', 'g');
+      pressKey('KeyH', 'h');
+      pressKey('KeyJ', 'j');
+      pressKey('KeyK', 'k');
+      pressKey('KeyL', 'l');
+      pressKey('Semicolon', ';');
+      pressKey('Quote', '"');
+      pressKey('Backslash', '|');
+      
+      pressKey('KeyZ', 'z');
+      pressKey('KeyX', 'x');
+      pressKey('KeyC', 'c');
+      pressKey('KeyV', 'v');
+      pressKey('KeyB', 'b');
+      pressKey('KeyN', 'n');
+      pressKey('KeyM', 'm');
+      pressKey('Comma', '<');
+      pressKey('Period', '>');
+      pressKey('Slash', '?');
+      pressKey('Quote', '"');
+      pressKey('Backslash', '|');
+
+      pressKey('Space', ' ');
+
+  
--- keys.js
+++ keys.js
@@ -11,72 +11,99 @@ row1.className = 'row';
 k.appendChild(row1);
 
 var q = document.createElement('button');
-q.className = 'keyQ';
+q.id = 'q'
 q.type = 'button';
+q.className = 'keyQ';
 q.innerHTML = '`'
 row1.appendChild(q);
 
-var q1 = document.createElement('div');
+var q1 = document.createElement('button');
+q1.id = 'q1'
+q1.type = 'button';
 q1.className = 'keyQ';
 q1.innerHTML = '1'
 row1.appendChild(q1);
 
-var q2 = document.createElement('div');
+var q2 = document.createElement('button');
+q2.id = 'q2'
+q2.type = 'button';
 q2.className = 'keyQ';
 q2.innerHTML = '2'
 row1.appendChild(q2);
 
-var q3 = document.createElement('div');
+var q3 = document.createElement('button');
+q3.id = 'q3'
+q3.type = 'button';
 q3.className = 'keyQ';
 q3.innerHTML = '3'
 row1.appendChild(q3);
 
-var q4 = document.createElement('div');
+var q4 = document.createElement('button');
+q4.id = 'q4'
+q4.type = 'button';
 q4.className = 'keyQ';
 q4.innerHTML = '4'
 row1.appendChild(q4);
 
-var q5 = document.createElement('div');
+var q5 = document.createElement('button');
+q5.id = 'q5'
+q5.type = 'button';
 q5.className = 'keyQ';
 q5.innerHTML = '5'
 row1.appendChild(q5);
 
-var q6 = document.createElement('div');
+var q6 = document.createElement('button');
+q6.id = 'q6'
+q6.type = 'button';
 q6.className = 'keyQ';
 q6.innerHTML = '6'
 row1.appendChild(q6);
 
-var q7 = document.createElement('div');
+var q7 = document.createElement('button');
+q7.id = 'q7'
+q7.type = 'button';
 q7.className = 'keyQ';
 q7.innerHTML = '7'
 row1.appendChild(q7);
 
-var q8 = document.createElement('div');
+var q8 = document.createElement('button');
+q8.id = 'q8'
+q8.type = 'button';
 q8.className = 'keyQ';
 q8.innerHTML = '8'
 row1.appendChild(q8);
 
-var q9 = document.createElement('div');
+var q9 =document.createElement('button');
+q9.id = 'q9'
+q9.type = 'button';
 q9.className = 'keyQ';
 q9.innerHTML = '9'
 row1.appendChild(q9);
 
-var q0 = document.createElement('div');
+var q0 = document.createElement('button');
+q0.id = 'q0'
+q0.type = 'button';
 q0.className = 'keyQ';
 q0.innerHTML = '0'
 row1.appendChild(q0);
 
-var q01 = document.createElement('div');
+var q01 = document.createElement('button');
+q01.id = 'q01'
+q01.type = 'button';
 q01.className = 'keyQ';
 q01.innerHTML = '-'
 row1.appendChild(q01);
 
-var q02 = document.createElement('div');
+var q02 = document.createElement('button');
+q02.id = 'q02'
+q02.type = 'button';
 q02.className = 'keyQ';
 q02.innerHTML = '='
 row1.appendChild(q02);
 
-var q03 = document.createElement('div');
+var q03 = document.createElement('button');
+q03.id = 'q03'
+q03.type = 'button';
 q03.className = 'keyB';
 q03.innerHTML = 'Backspace'
 row1.appendChild(q03);
@@ -85,72 +112,100 @@ var row2 = document.createElement('div');
 row2.className = 'row';
 k.appendChild(row2);
 
-var a = document.createElement('div');
+var a = document.createElement('button');
+a.id = 'a'
+a.type = 'button';
 a.className = 'tab';
 a.innerHTML = 'TAB'
 row2.appendChild(a);
 
-var a1 = document.createElement('div');
+var a1 = document.createElement('button');
+a1.id = 'a1'
+a1.type = 'button';
 a1.className = 'keyQ';
 a1.innerHTML = 'Q'
 row2.appendChild(a1);
 
-var a2 = document.createElement('div');
+var a2 = document.createElement('button');
+a2.id = 'a2'
+a2.type = 'button';
 a2.className = 'keyQ';
 a2.innerHTML = 'W'
 row2.appendChild(a2);
 
-var a3 = document.createElement('div');
+var a3 = document.createElement('button');
+a3.id = 'a3'
+a3.type = 'button';
 a3.className = 'keyQ';
 a3.innerHTML = 'E'
 row2.appendChild(a3);
 
-var a4 = document.createElement('div');
+var a4 = document.createElement('button');
+a4.id = 'a4'
+a4.type = 'button';
 a4.className = 'keyQ';
 a4.innerHTML = 'R';
 row2.appendChild(a4);
 
-var a5 = document.createElement('div');
+var a5 = document.createElement('button');
+a5.id = 'a5'
+a5.type = 'button';
 a5.className = 'keyQ';
 a5.innerHTML = 'T'
 row2.appendChild(a5);
 
-var a6 = document.createElement('div');
+var a6 = document.createElement('button');
+a6.id = 'a6'
+a6.type = 'button';
 a6.className = 'keyQ';
 a6.innerHTML = 'Y'
 row2.appendChild(a6);
 
-var a7 = document.createElement('div');
+var a7 = document.createElement('button');
+a7.id = 'a7'
+a7.type = 'button';
 a7.className = 'keyQ';
 a7.innerHTML = 'U'
 row2.appendChild(a7);
 
-var a8 = document.createElement('div');
+var a8 = document.createElement('button');
+a8.id = 'a8'
+a8.type = 'button';
 a8.className = 'keyQ';
 a8.innerHTML = 'I'
 row2.appendChild(a8);
 
-var a9 = document.createElement('div');
+var a9 = document.createElement('button');
+a9.id = 'a9'
+a9.type = 'button';
 a9.className = 'keyQ';
 a9.innerHTML = 'O'
 row2.appendChild(a9);
 
-var a0 = document.createElement('div');
+var a0 = document.createElement('button');
+a0.id = 'a0'
+a0.type = 'button';
 a0.className = 'keyQ';
 a0.innerHTML = 'P'
 row2.appendChild(a0);
 
-var a01 = document.createElement('div');
+var a01 = document.createElement('button');
+a01.id = 'a01'
+a01.type = 'button';
 a01.className = 'keyQ';
 a01.innerHTML = '['
 row2.appendChild(a01);
 
-var a02 = document.createElement('div');
+var a02 = document.createElement('button');
+a02.id = 'a02'
+a02.type = 'button';
 a02.className = 'keyQ';
 a02.innerHTML = ']'
 row2.appendChild(a02);
 
-var a03 = document.createElement('div');
+var a03 = document.createElement('button');
+a03.id = 'a03'
+a03.type = 'button';
 a03.className = 'enter';
 a03.innerHTML = 'ENTER'
 row2.appendChild(a03);
@@ -159,72 +214,100 @@ var row3 = document.createElement('div');
 row3.className = 'row';
 k.appendChild(row3);
 
-var b = document.createElement('div');
+var b = document.createElement('button');
+b.id = 'b'
+b.type = 'button';
 b.className = 'keyB';
 b.innerHTML = 'Caps Lock'
 row3.appendChild(b);
 
-var b1 = document.createElement('div');
+var b1 = document.createElement('button');
+b1.id = 'b1'
+b1.type = 'button';
 b1.className = 'keyQ';
 b1.innerHTML = 'A'
 row3.appendChild(b1);
 
-var b2 = document.createElement('div');
+var b2 = document.createElement('button');
+b2.id = 'b2'
+b2.type = 'button';
 b2.className = 'keyQ';
 b2.innerHTML = 'S'
 row3.appendChild(b2);
 
-var b3 = document.createElement('div');
+var b3 = document.createElement('button');
+b3.id = 'b3'
+b3.type = 'button';
 b3.className = 'keyQ';
 b3.innerHTML = 'D'
 row3.appendChild(b3);
 
-var b4 = document.createElement('div');
+var b4 = document.createElement('button');
+b4.id = 'b4'
+b4.type = 'button';
 b4.className = 'keyQ';
 b4.innerHTML = 'F';
 row3.appendChild(b4);
 
-var b5 = document.createElement('div');
+var b5 = document.createElement('button');
+b5.id = 'b5'
+b5.type = 'button';
 b5.className = 'keyQ';
 b5.innerHTML = 'G'
 row3.appendChild(b5);
 
-var b6 = document.createElement('div');
+var b6 = document.createElement('button');
+b6.id = 'b6'
+b6.type = 'button';
 b6.className = 'keyQ';
 b6.innerHTML = 'H'
 row3.appendChild(b6);
 
-var b7 = document.createElement('div');
+var b7 = document.createElement('button');
+b7.id = 'b7'
+b7.type = 'button';
 b7.className = 'keyQ';
 b7.innerHTML = 'J'
 row3.appendChild(b7);
 
-var b8 = document.createElement('div');
+var b8 = document.createElement('button');
+b8.id = 'b8'
+b8.type = 'button';
 b8.className = 'keyQ';
 b8.innerHTML = 'K'
 row3.appendChild(b8);
 
-var b9 = document.createElement('div');
+var b9 = document.createElement('button');
+b9.id = 'b9'
+b9.type = 'button';
 b9.className = 'keyQ';
 b9.innerHTML = 'L'
 row3.appendChild(b9);
 
-var b0 = document.createElement('div');
+var b0 = document.createElement('button');
+b0.id = 'b0'
+b0.type = 'button';
 b0.className = 'keyQ';
 b0.innerHTML = ':'
 row3.appendChild(b0);
 
-var b01 = document.createElement('div');
+var b01 = document.createElement('button');
+b01.id = 'b01'
+b01.type = 'button';
 b01.className = 'keyQ';
 b01.innerHTML = '"'
 row3.appendChild(b01);
 
-var b02 = document.createElement('div');
+var b02 = document.createElement('button');
+b02.id = 'b02'
+b02.type = 'button';
 b02.className = 'keyQ';
 b02.innerHTML = '|';
 row3.appendChild(b02);
 
-var b03 = document.createElement('div');
+var b03 = document.createElement('button');
+b03.id = 'b03'
+b03.type = 'button';
 b03.className = 'keyQ';
 b03.innerHTML = '~';
 row3.appendChild(b03);
@@ -233,72 +316,100 @@ var row4 = document.createElement('div');
 row4.className = 'row';
 k.appendChild(row4);
 
-var c = document.createElement('div');
+var c = document.createElement('button');
+c.id = 'c'
+c.type = 'button';
 c.className = 'keyB';
 c.innerHTML = 'Shift'
 row4.appendChild(c);
 
-var c1 = document.createElement('div');
+var c1 = document.createElement('button');
+c1.id = 'c1'
+c1.type = 'button';
 c1.className = 'keyQ';
 c1.innerHTML = 'Z'
 row4.appendChild(c1);
 
-var c2 = document.createElement('div');
+var c2 = document.createElement('button');
+c2.id = 'c2'
+c2.type = 'button';
 c2.className = 'keyQ';
 c2.innerHTML = 'X'
 row4.appendChild(c2);
 
-var c3 = document.createElement('div');
+var c3 = document.createElement('button');
+c3.id = 'c3'
+c3.type = 'button';
 c3.className = 'keyQ';
 c3.innerHTML = 'C'
 row4.appendChild(c3);
 
-var c4 = document.createElement('div');
+var c4 = document.createElement('button');
+c4.id = 'c4'
+c4.type = 'button';
 c4.className = 'keyQ';
 c4.innerHTML = 'V';
 row4.appendChild(c4);
 
-var c5 = document.createElement('div');
+var c5 = document.createElement('button');
+c5.id = 'c5'
+c5.type = 'button';
 c5.className = 'keyQ';
 c5.innerHTML = 'B'
 row4.appendChild(c5);
 
-var c6 = document.createElement('div');
+var c6 = document.createElement('button');
+c6.id = 'c6'
+c6.type = 'button';
 c6.className = 'keyQ';
 c6.innerHTML = 'N'
 row4.appendChild(c6);
 
-var c7 = document.createElement('div');
+var c7 = document.createElement('button');
+c7.id = 'c7'
+c7.type = 'button';
 c7.className = 'keyQ';
 c7.innerHTML = 'M'
 row4.appendChild(c7);
 
-var c8 = document.createElement('div');
+var c8 = document.createElement('button');
+c8.id = 'c8'
+c8.type = 'button';
 c8.className = 'keyQ';
 c8.innerHTML = '<'
 row4.appendChild(c8);
 
-var c9 = document.createElement('div');
+var c9 = document.createElement('button');
+c9.id = 'c9'
+c9.type = 'button';
 c9.className = 'keyQ';
 c9.innerHTML = '>'
 row4.appendChild(c9);
 
-var c0 = document.createElement('div');
+var c0 = document.createElement('button');
+c0.id = 'c0'
+c0.type = 'button';
 c0.className = 'keyQ';
 c0.innerHTML = '?'
 row4.appendChild(c0);
 
-var c01 = document.createElement('div');
+var c01 = document.createElement('button');
+c01.id = 'c01'
+c01.type = 'button';
 c01.className = 'keyQ';
 c01.innerHTML = '*'
 row4.appendChild(c01);
 
-var c02 = document.createElement('div');
+var c02 = document.createElement('button');
+c02.id = 'c02'
+c02.type = 'button';
 c02.className = 'keyQ';
 c02.innerHTML = '+';
 row4.appendChild(c02);
 
-var c03 = document.createElement('div');
+var c03 = document.createElement('button');
+c03.id = 'c03'
+c03.type = 'button';
 c03.className = 'keyQ';
 c03.innerHTML = '_';
 row4.appendChild(c03);
@@ -307,32 +418,44 @@ var row5 = document.createElement('div');
 row5.className = 'row';
 k.appendChild(row5);
 
-var d = document.createElement('div');
+var d = document.createElement('button');
+d.id = 'd'
+d.type = 'button';
 d.className = 'keyB';
 d.innerHTML = 'Ctr'
 row5.appendChild(d);
 
-var d1 = document.createElement('div');
+var d1 = document.createElement('button');
+d1.id = 'd1'
+d1.type = 'button';
 d1.className = 'keyQ';
 d1.innerHTML = 'Win'
 row5.appendChild(d1);
 
-var d2 = document.createElement('div');
+var d2 = document.createElement('button');
+d2.id = 'd2'
+d2.type = 'button';
 d2.className = 'keyQ';
 d2.innerHTML = 'Alt'
 row5.appendChild(d2);
 
-var d3 = document.createElement('div');
+var d3 = document.createElement('button');
+d3.id = 'd3'
+d3.type = 'button';
 d3.className = 'keySpace';
 d3.innerHTML = 'SPACE'
 row5.appendChild(d3);
 
-var d4 = document.createElement('div');
+var d4 = document.createElement('button');
+d4.id = 'd4'
+d4.type = 'button';
 d4.className = 'keyQ';
 d4.innerHTML = 'Alt';
 row5.appendChild(d4);
 
-var d5 = document.createElement('div');
+var d5 = document.createElement('button');
+d5.id = 'd5'
+d5.type = 'button';
 d5.className = 'keyB';
 d5.innerHTML = 'Ctr'
 row5.appendChild(d5);
